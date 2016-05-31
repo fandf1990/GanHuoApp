dd66d3d6c1f9f38a8097dcee9fac49eda72de94f
fandf1990 <fandongfeng1990@foxmail.com>
2016-05-31 17:43:17 +0800

    first commit,init project.

---
+++ app/src/main/java/app/df/gank/io/ganhuo/MainActivity.java
@@ -0,0 +1,13 @@
+package app.df.gank.io.ganhuo;
+
+import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
