1b16ddafb976011946cf2f5f4de8773b0861486e
feng24k <fandongfeng1990@foxmail.com>
2016-06-14 20:53:42 +0800

    修改launcher图标

--- app/src/main/java/io/gank/feng24k/app/ui/activity/ResourceDetailActivity.java
+++ app/src/main/java/io/gank/feng24k/app/ui/activity/ResourceDetailActivity.java
@@ -5,7 +5,11 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 import io.gank.feng24k.app.R;
 import io.gank.feng24k.app.ui.base.BaseActivity;
@@ -15,6 +19,7 @@ public class ResourceDetailActivity extends BaseActivity{
     public static final String INTENT_RESOURCE_DETAIL_CODE = "intent_resource_detail_code";
     private String mContent;
     private WebView mWebView;
+    private ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -22,8 +27,25 @@ public class ResourceDetailActivity extends BaseActivity{
         mContent = getIntent().getStringExtra(INTENT_RESOURCE_DETAIL_CODE);
         setContentView(R.layout.resource_detail_activity, null);
         mWebView = (WebView) findViewById(R.id.resource_detail_webview);
+        mProgressBar = (ProgressBar) findViewById(R.id.resource_detail_progress_bar);
         mWebView.getSettings().setJavaScriptEnabled(true);
-        if (mContent.startsWith("http")) {
+
+        mWebView.setWebChromeClient(new WebChromeClient(){
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+                mProgressBar.setProgress(newProgress);
+            }
+
+        });
+        mWebView.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                mProgressBar.setVisibility(View.GONE);
+            }
+        });
+        if (mContent.startsWith("http")||mContent.startsWith("https")) {
             mWebView.loadUrl(mContent);
         } else {
             mWebView.loadDataWithBaseURL(null, mContent, "text/html", "UTF-8", null);
